df54b04a051d6a622edbe6a0f514754e77617ab8
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2023-09-27 17:22:57 -0300

    Fixed mobile responsive

--- src/Sections/Experience.jsx
+++ src/Sections/Experience.jsx
@@ -1,9 +1,9 @@
 
 const Experience = () => {
   return (
-    <div className="flex justify-evenly xl:mx-64 mx-32 my-28 gap-20">
+    <div className="flex xl:flex-row flex-col justify-evenly xl:mx-64 mx-16 my-28 xl:gap-20 gap-4">
         <h1 className="text-2xl text-light-brown whitespace-nowrap">Experience the Aroma</h1>
-        <p className="text-bone mx-10">Our coffee shop, Café Aroma, offers the finest blends to kickstart your morning or rejuvenate your afternoon. Soak in the captivating atmosphere as you savor every sip of our enchanting brews.</p>
+        <p className="text-bone xl:mx-10">Our coffee shop, Café Aroma, offers the finest blends to kickstart your morning or rejuvenate your afternoon. Soak in the captivating atmosphere as you savor every sip of our enchanting brews.</p>
     </div>
   )
 }
